aed511520d349de66540828a2d2502ab42b4a7b1
TribQq <ghhghdffgj@gmail.com>
2022-04-15 13:43:05 +0500

    FILE_STORAGE

--- mysite/settings.py
+++ mysite/settings.py
@@ -115,7 +115,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
@@ -125,7 +127,7 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': config('API_SECRET', default=''),
 }
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE = config('FILE_STORAGE')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
